a17852b82391e7dc9fb09ff2bf949b46ecc1c119
kyslynsky <kyslynsky@gmail.com>
2022-05-19 18:10:58 +0300

    Task done

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,1 +1,17 @@
-function fetchCountries(name) {}
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { ERROR_MSG } from './index';
+
+const BASE_URL = 'https://restcountries.com/v3.1/name';
+
+export function fetchCountries(name) {
+  return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`).then(
+    responce => {
+      if (responce.ok) {
+        return responce.json();
+      }
+
+      Notify.failure(`${ERROR_MSG}`, { clickToClose: true });
+      throw new Error(responce.statusText);
+    },
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,81 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
+export const ERROR_MSG = 'Oops, there is no country with that name';
+
+const refs = {
+  searchInput: document.querySelector('#search-box'),
+  countyList: document.querySelector('.country-list'),
+  infoBlock: document.querySelector('.country-info'),
+};
+
+refs.searchInput.addEventListener('input', debounce(onCountrySearch, DEBOUNCE_DELAY));
+
+function onCountrySearch(e) {
+  const query = e.target.value.trim();
+
+  if (query === '') {
+    deleteMarkup(refs.infoBlock);
+    deleteMarkup(refs.countyList);
+    return;
+  }
+
+  fetchCountries(query)
+    .then(data => {
+      if (data.length > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.', {
+          clickToClose: true,
+        });
+        return;
+      }
+
+      renderMarkup(data);
+    })
+    .catch(error => {
+      deleteMarkup(refs.infoBlock);
+      deleteMarkup(refs.countyList);
+      console.log(error);
+    });
+}
+
+function renderMarkup(matches) {
+  if (matches.length > 1) {
+    refs.countyList.innerHTML = makeSearchPreviewMarkup(matches);
+    deleteMarkup(refs.infoBlock);
+  } else {
+    refs.infoBlock.innerHTML = makeInfoBlockMarkup(matches);
+    deleteMarkup(refs.countyList);
+  }
+}
+
+function makeSearchPreviewMarkup(matches) {
+  return matches
+    .map(
+      ({ flags, name }) =>
+        `<li><img src="${flags.svg}" alt="${name.official}" width="60" height="40"> ${name.official}</li>`,
+    )
+    .join('');
+}
+
+function makeInfoBlockMarkup(matches) {
+  return matches
+    .map(
+      ({ name, capital, population, flags, languages }) =>
+        `<h1><img src="${flags.svg}" alt="${name.official}" width="110px" height="80px"/>${
+          name.official
+        }</h1>
+        <ul>
+          <li>Capital: ${capital}</li>
+          <li>Population: ${population}</li>
+          <li>Languages: ${Object.values(languages)}</li>
+        </ul>`,
+    )
+    .join('');
+}
+
+function deleteMarkup(element) {
+  element.innerHTML = '';
+}
